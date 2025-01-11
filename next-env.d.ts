ca4a319acf8537ae9c08922f7bd7face8998bb8a
johndoe <alvarezleonardo2392@gmail.com>
2025-01-11 14:23:40 -0300

    VULNERABILIDAD SAST

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,5 +1,10 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
+
+
+const userInput = "console.log('This is unsafe')";
+eval(userInput); 
+
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/basic-features/typescript for more information.
